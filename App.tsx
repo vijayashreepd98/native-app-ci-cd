5f58e92bb4e7331bd80ed2861a6142ffae55409e
Vijayashree <vijayashree-at-411977029542>
2023-12-27 16:24:49 +0530

    fixed

--- App.tsx
+++ App.tsx
@@ -78,7 +78,7 @@ function App(): React.JSX.Element {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            now screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
